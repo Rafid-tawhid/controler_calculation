a1921187fcd3dd60683bf65e463141bfda545a55
Mobile Apps <rafidtawhid@gmail.com>
2021-10-03 11:33:05 +0600

    Stack,Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/home.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "BMI Calculator",
+        title: "Apps",
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.yellow,
@@ -22,4 +22,115 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class HomePage extends StatefulWidget {
+
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("AppBar"),
+        backgroundColor: Colors.red,
+        elevation: 1,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+
+        ],
+
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text("Heading"),decoration: BoxDecoration(color: Colors.red),),
+            ListTile(title: Text("Rafid"),onTap: (){},),
+            ListTile(title: Text("Tawhid"),onTap: (){},),
+
+          ],
+
+        ),
+
+      ),
+      body: ListView(
+
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Stack(
+                  alignment: Alignment.center,
+                  overflow: Overflow.visible,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Positioned(
+                      bottom: -20,
+                        child: CircleAvatar(radius: 25,backgroundColor: Colors.blueAccent,))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.chat),title: Text("Chat")),
+          BottomNavigationBarItem(icon: Icon(Icons.home),title: Text("Home")),
+          BottomNavigationBarItem(icon: Icon(Icons.person),title: Text("Profile")),
+
+        ],
+
+
+
+      ),
+    );
+  }
+}
+
+
 
